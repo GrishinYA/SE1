ad3366477c307508ed22dbc39446c331472de96c
GrishinYA <119912102+GrishinYA@users.noreply.github.com>
2022-12-07 08:42:07 +0500

    Add files via upload

---
+++ RepoA/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+//
+// This program is just an example one under VCS.
+// Author: Gtishin Yaroslav KE-417
+// учебная програма
+#include <iostream>
+int main()
+{
+int a, b;
+cout << "Enter A and B: ";
+cin >> a >> b;
+cout << "A + B = " << a + b << '\n'
+<< "A - B = " << a - b << '\n'
+<< "A * B = " << a / b << '\n';
+return 0;
+}
